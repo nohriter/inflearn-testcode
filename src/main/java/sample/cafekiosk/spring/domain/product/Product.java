3e5f4926c81ad5ceb98e4aa332c51beb4ab41072
nohriter <nohriter@gmail.com>
2024-07-07 17:41:32 +0900

    :sparkles: Persistence Layer 테스트 (2)
    - `@DataJpaTest`와 `@SpringBootTest`의 차이 간단비교
    - `@activeprofiles` 지정
    - List 검증
      - contains
      - containsExactly
      - containsExactlyInAnyOrder

--- src/main/java/sample/cafekiosk/spring/domain/product/Product.java
+++ src/main/java/sample/cafekiosk/spring/domain/product/Product.java
@@ -6,10 +6,14 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import sample.cafekiosk.spring.domain.BaseEntity;
 
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class Product extends BaseEntity {
 
@@ -28,4 +32,14 @@ public class Product extends BaseEntity {
 	private String name;
 
 	private int price;
+
+	@Builder
+	private Product(String productNumber, ProductType type, ProductSellingStatus sellingStatus,
+		String name, int price) {
+		this.productNumber = productNumber;
+		this.type = type;
+		this.sellingStatus = sellingStatus;
+		this.name = name;
+		this.price = price;
+	}
 }
